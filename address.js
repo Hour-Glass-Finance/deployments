f1df191205e91699e718abcfe95b987a99763bfa
hourglass <hourglassfinance@protonmail.com>
2021-07-04 17:22:57 +0100

    update new SwapMiningRewards LiquidityMiningRewards Treasury hourUSD

--- address.js
+++ address.js
@@ -2,14 +2,14 @@ module.exports = {
   "137": {
     "HOUR": "0x04Dc2Da39759B272A6bEC64e6771EA0b87737151",
     // hourUSD is an ERC20 token and also the hourglasss usd pool
-    "hourUSD": "0x14B99c2f51A09FCeeD5c9FEa0548d75faE0a933D",
+    "hourUSD": "0xab165fc2Fca90AF2adfb019D0363fe3091B5EcD4",
     "Converter": "0x5076b6F827DA928697D61D69Ec2E1E6c1037cA2D",
     "FeeDistributor": "0x967E8507a0951d9B432d62d7438562006d0eEeaE",
     "Vault": "0xA34feb066337A44Da3FBED0e71E3a3dcaC97c363",
     "SwapMiningRewardsCalc": "0xE7dA346E80544216eB2B79f9F353b31281192a21",
-    "SwapMiningRewards": "0xEb7909F83b9C8E9a1DA730806722AF7f4e2E6128",
-    "LiquidityMiningRewards": "0xE4c1Aa90B29A9Fb775266edb85257b363F5C2cb7",
-    "Treasury": "0x2dCEAAC2Ba582e56Dd072307deCBA3fE502cce61",
+    "SwapMiningRewards": "0x62456D411Ca941a9fc7511831A52A12Eb8ee80c0",
+    "LiquidityMiningRewards": "0xf81E859675A8f8baAe1b30Dd89062Daa68a5C5b8",
+    "Treasury": "0xd358A2BFdc757Ae6456A0EB2ea687E47C5CAC400",
     "USDC/HOUR SUSHI-LP": "0x182af06fBA5aC90e538D25244D5bcC615208bbc9"
   }
 }
