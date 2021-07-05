7d95099ccb7fbd9f3324bd0f613beeabade0837f
hourglass <hourglassfinance@protonmail.com>
2021-07-05 20:46:50 +0100

    update hourUSD address

--- address.js
+++ address.js
@@ -2,7 +2,7 @@ module.exports = {
   "137": {
     "HOUR": "0x04Dc2Da39759B272A6bEC64e6771EA0b87737151",
     // hourUSD is an ERC20 token and also the hourglasss usd pool
-    "hourUSD": "0xab165fc2Fca90AF2adfb019D0363fe3091B5EcD4",
+    "hourUSD": "0x9F57A730b8E806A3bFFfd48aec2F84c77aA9c9E9",
     "Converter": "0x5076b6F827DA928697D61D69Ec2E1E6c1037cA2D",
     "FeeDistributor": "0x967E8507a0951d9B432d62d7438562006d0eEeaE",
     "Vault": "0xA34feb066337A44Da3FBED0e71E3a3dcaC97c363",
